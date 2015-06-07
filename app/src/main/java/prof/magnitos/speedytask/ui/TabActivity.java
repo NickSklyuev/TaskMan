ba383bb06b381ba564f70df5b8c45b025713a13e
Yuriy Gagarin <service.ru@mail.ru>
2015-06-07 18:56:34 +0500

    chart

--- app/src/main/java/prof/magnitos/speedytask/ui/TabActivity.java
+++ app/src/main/java/prof/magnitos/speedytask/ui/TabActivity.java
@@ -4,14 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import com.parse.ParseAnalytics;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 import prof.magnitos.speedytask.R;
 import prof.magnitos.speedytask.services.MainService;
@@ -27,7 +28,17 @@ public class TabActivity extends ActionBarActivity {
         setContentView(R.layout.activity_tab);
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
-        
+
+        ParsePush.subscribeInBackground("Everyone", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
 
         Intent main_serveice = new Intent(this,MainService.class);
         main_serveice.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
